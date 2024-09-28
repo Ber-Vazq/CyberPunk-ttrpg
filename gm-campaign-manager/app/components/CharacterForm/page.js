16343d7103c0da2ff435c7e58c1fc038075e174c
Ber-Vazq <bvaq20@hotmail.com>
2024-09-28 18:21:23 -0500

    updating the form, to look better

--- gm-campaign-manager/app/components/CharacterForm/page.js
+++ gm-campaign-manager/app/components/CharacterForm/page.js
@@ -2,11 +2,12 @@
 import React, { useState } from 'react';
 import '../../globals.css';
 import axios from 'axios';
-import Cyberware from '../Cyberware';
+import Cyberware from '../Cyberware/page';
 
 const CharacterForm = ({ campaignId, onCharacterCreated }) => {
   const [name, setName] = useState('');
   const [role, setRole] = useState('');
+  const [roleAbility, setRoleAbility] = useState(''); // Role ability state
   const [stats, setStats] = useState({
     intelligence: 0,
     reflex: 0,
@@ -23,11 +24,58 @@ const CharacterForm = ({ campaignId, onCharacterCreated }) => {
   const [cyberware, setCyberware] = useState([]);
   const [inventory, setInventory] = useState([]);
 
+  const rolesData = {
+    "Solo": "Combat Sense",
+    "Netrunner": "Interface",
+    "Tech": "Maker",
+    "Media": "Credibility",
+    "Fixer": "Streetdeal",
+    "Nomad": "Moto",
+    "Cop": "Authority",
+    "Rockerboy": "Charismatic Impact",
+    "Medtech": "Medicine",
+    "Exec": "Teamwork",
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const characterData = { name, role, stats, health, cyberware, inventory, campaign_id: campaignId };
+
+    const characterData = { name, role, roleAbility, stats, health, cyberware, inventory, campaign_id: campaignId };
     const res = await axios.post('http://localhost:3001/api/character', characterData);
     onCharacterCreated(res.data);
+  };
+  
+
+  const calculateMaxHP = (body, willpower) => {
+    const healthTable = [
+      [20, 25, 25, 30, 30, 35, 35, 40, 40, 45, 45, 50, 50, 55],
+      [25, 25, 30, 30, 35, 35, 40, 40, 45, 45, 50, 50, 55, 55],
+      [25, 30, 30, 35, 35, 40, 40, 45, 45, 50, 50, 55, 55, 60],
+      [30, 30, 35, 35, 40, 40, 45, 45, 50, 50, 55, 55, 60, 60],
+      [30, 35, 35, 40, 40, 45, 45, 50, 50, 55, 55, 60, 60, 65],
+      [35, 35, 40, 40, 45, 45, 50, 50, 55, 55, 60, 60, 65, 65],
+      [35, 40, 40, 45, 45, 50, 50, 55, 55, 60, 60, 65, 65, 70],
+      [40, 40, 45, 45, 50, 50, 55, 55, 60, 60, 65, 65, 70, 70],
+      [40, 45, 45, 50, 50, 55, 55, 60, 60, 65, 65, 70, 70, 75],
+      [45, 45, 50, 50, 55, 55, 60, 60, 65, 65, 70, 70, 75, 75],
+    ];
+
+    const bodyIndex = Math.min(Math.max(body - 2, 0), 13);
+    const willIndex = Math.min(Math.max(willpower - 2, 0), 9);
+
+    return healthTable[willIndex][bodyIndex];
+  };
+  
+
+  const handleStatChange = (stat, value) => {
+    const updatedStats = { ...stats, [stat]: Number(value) };
+
+    if (stat === 'body' || stat === 'willpower') {
+      const newMaxHP = calculateMaxHP(updatedStats.body, updatedStats.willpower);
+      setHealth({ ...health, max_hp: newMaxHP, current_hp: newMaxHP });
+    }
+
+    setStats(updatedStats);
   };
 
   return (
@@ -35,29 +83,46 @@ const CharacterForm = ({ campaignId, onCharacterCreated }) => {
       <h2 className="text-3xl text-cyberpunk-orange font-bold mb-4 text-center">Create Your Cyberpunk Character</h2>
 
       {/* Character Information */}
-      <div className="mb-6">
-        <div className="flex justify-between">
-          <div className="w-1/2 pr-4">
-            <label className="block text-cyberpunk-pink mb-1">Character Name</label>
-            <input
-              type="text"
-              placeholder="Character Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
-            />
-          </div>
-          <div className="w-1/2">
-            <label className="block text-cyberpunk-pink mb-1">Role</label>
-            <input
-              type="text"
-              placeholder="Role"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-              className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
-            />
-          </div>
+      <div className="flex mb-6">
+        <div className="w-1/2 pr-4">
+          <label className="block text-cyberpunk-pink mb-1">Character Name</label>
+          <input
+            type="text"
+            placeholder="Character Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
+          />
         </div>
+
+        <div className="w-1/2">
+          <label className="block text-cyberpunk-pink mb-1">Role</label>
+          <select
+            value={role}
+            onChange={(e) => {
+              setRole(e.target.value);
+              setRoleAbility(rolesData[e.target.value]);
+            }}
+            className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
+          >
+            <option value="">Select Role</option>
+            {Object.keys(rolesData).map((roleName) => (
+              <option key={roleName} value={roleName}>
+                {roleName}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      <div className="w-1/2 mb-6">
+        <label className="block text-cyberpunk-pink mb-1">Role Ability</label>
+        <input
+          type="text"
+          readOnly
+          value={roleAbility}
+          className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
+        />
       </div>
 
       {/* Stats Section */}
@@ -70,7 +135,7 @@ const CharacterForm = ({ campaignId, onCharacterCreated }) => {
               <input
                 type="number"
                 value={stats[stat]}
-                onChange={(e) => setStats({ ...stats, [stat]: Number(e.target.value) })}
+                onChange={(e) => handleStatChange(stat, e.target.value)}
                 className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
               />
             </div>
@@ -80,26 +145,24 @@ const CharacterForm = ({ campaignId, onCharacterCreated }) => {
 
       {/* Health Section */}
       <div className="mb-6">
-        <h3 className="text-xl text-cyberpunk-orange font-bold mb-2">Health</h3>
-        <div className="flex justify-between">
-          <div className="w-1/2 pr-4">
-            <label className="block text-cyberpunk-pink mb-1">Max HP</label>
-            <input
-              type="number"
-              placeholder="Max HP"
-              value={health.max_hp}
-              onChange={(e) => setHealth({ ...health, max_hp: Number(e.target.value) })}
-              className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
-            />
-          </div>
+        <h3 className="text-xl text-cyberpunk-orange font-bold mb-2">Character Health</h3>
+        <div>
+          <label className="block text-cyberpunk-pink mb-1">Max HP</label>
+          <input
+            type="number"
+            value={health.max_hp}
+            readOnly
+            className="w-full px-3 py-2 border border-cyberpunk-pink bg-cyberpunk-blue text-cyberpunk-pink rounded-lg focus:outline-none"
+          />
         </div>
       </div>
 
-      {/* Cyberware and Inventory */}
+      {/* Cyberware Section */}
       <div className="mb-6">
-      <Cyberware cyberware={cyberware} setCyberware={setCyberware} />
+        <Cyberware cyberware={cyberware} setCyberware={setCyberware} />
       </div>
 
+      {/* Inventory Section */}
       <div className="mb-6">
         <h3 className="text-xl text-cyberpunk-orange font-bold mb-2">Inventory</h3>
         <textarea
@@ -111,9 +174,10 @@ const CharacterForm = ({ campaignId, onCharacterCreated }) => {
         ></textarea>
       </div>
 
+      {/* Submit Button */}
       <button
         type="submit"
-        className="w-full py-3 mt-6 bg-cyberpunk-orange text-cyberpunk-black font-bold rounded-lg hover:bg-cyberpunk-pink hover:text-cyberpunk-black"
+        className="w-full bg-cyberpunk-orange text-white py-2 rounded-lg font-bold hover:bg-cyberpunk-pink focus:outline-none"
       >
         Create Character
       </button>
